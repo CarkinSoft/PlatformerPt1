1b580a4a8527da19b9968c40126f1d468976fac2
CarkinOtter <carcardoza@csumb.edu>
2026-02-25 21:07:55 -0800

    Update CharacterDriver.cs

--- Assets/Scripts/CharacterDriver.cs
+++ Assets/Scripts/CharacterDriver.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,14 +8,20 @@ public class CharacterDriver : MonoBehaviour
     public float runSpeed = 10f;
     public float groundAcceleration = 15f;
 
+    public float apexHeight = 4.5f;
+    public float apexTime = 0.5f;
+    
     Vector2 _velocity;
     CharacterController _controller;
+    Quaternion facingRight;
+    Quaternion facingLeft;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
-        
+    void Start()
+    {
+        facingRight = Quaternion.identity;
+        facingLeft = Quaternion.Euler(0f, 180f, 0f);
         _controller = GetComponent<CharacterController>();
-        
     }
 
     // Update is called once per frame
@@ -23,17 +30,52 @@ public class CharacterDriver : MonoBehaviour
         float direction = 0f;
         if (Keyboard.current.dKey.isPressed) direction += 1f;
         if (Keyboard.current.aKey.isPressed) direction -= 1f;
+        bool jumpPressedThisFrame = Keyboard.current.spaceKey.wasPressedThisFrame;
+        bool jumpHeld = Keyboard.current.spaceKey.isPressed;
 
-        _velocity.x += groundAcceleration * Time.deltaTime;
-        if (direction != 0f)
+        float gravityModifier = 1f;
+        
+        //_velocity.x += groundAcceleration * Time.deltaTime;
+        if (_controller.isGrounded)
         {
-            _velocity.x = direction * groundAcceleration * Time.deltaTime;
-            _velocity.x = Mathf.Clamp(_velocity.x, -walkSpeed, walkSpeed);
-        }
-        else _velocity.x = 0f;
+            if (direction != 0f)
+            {
+                if (Mathf.Sign(direction) != Mathf.Sign(_velocity.x))
+                    _velocity.x = 0f;
 
-        float deltaX = direction * walkSpeed * Time.deltaTime;
-        Vector3 deltaPosition = new(deltaX, 0f, 0f);
-        _controller.Move(deltaPosition);
+                _velocity.x += direction * groundAcceleration * Time.deltaTime;
+                _velocity.x = Mathf.Clamp(_velocity.x, -walkSpeed, walkSpeed);
+                
+                transform.rotation = (direction > 0f) ? facingRight : facingLeft;
+            }
+            else
+            {
+                //_velocity.x *= 1f - Time.deltaTime * 2f;
+                _velocity.x = Mathf.MoveTowards(_velocity.x, 0f, groundAcceleration * Time.deltaTime);
+            }
+
+            if (jumpPressedThisFrame)
+                _velocity.y = 2f * apexHeight / apexTime;
+        }
+        else
+        {
+            if (!jumpHeld){
+                gravityModifier = 2f;
+                
+            }
+        }
+        
+        float gravity = 2f * apexHeight / (apexTime * apexTime);
+        _velocity.y -= gravity * gravityModifier * Time.deltaTime;
+        
+        float deltaX = _velocity.x * Time.deltaTime;
+        float deltaY = _velocity.y * Time.deltaTime;
+        
+        Vector3 deltaPosition = new(deltaX, deltaY, 0f);
+        CollisionFlags collisions = _controller.Move(deltaPosition);
+        if ((collisions & CollisionFlags.CollidedAbove) != 0)
+            _velocity.y = -1f;
+        if ((collisions & CollisionFlags.CollidedSides) != 0)
+            _velocity.x = 0f;
     }
 }
